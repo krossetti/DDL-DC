46f445506b7b83a09122d80b1d3dd83de23b9da8
krossetti <ktr24@georgetown.edu>
2016-03-16 00:17:30 -0400

    Added files via upload

--- WHvisitorLOG_wrangle.py
+++ WHvisitorLOG_wrangle.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 """
 This code wrangles the White House visitor logs into normalized data to
-push into the DB
+push into the DB. The attributes being modified are parsing the DATE & TIME into
+separate fields
 
 """
 ##########################################################################
@@ -9,6 +10,27 @@ push into the DB
 ##########################################################################
 
 import datetime # required for
+import dateutil # required for normalization of the date
+import psycopg2 #required to access psql db
+import csv 
+
+
+
+#db parameters
+DB = 'WhiteHouse.Visitors'
+user = 'postgres'
+password = 'psqlpass'
+host = 'localhost'
+port = '5432'
+
+##########################################################################
+## Functions
+##########################################################################
+
+#connect to the db
+conn_str="host={} port={} dbname={} user={} password={}".format(host, port, DB, user, password)
+conn=psycopg2.connect(conn_str)
+c=conn.cursor()
 
 
 ##########################################################################
